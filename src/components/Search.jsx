446d5d5246c4b9f3062c0295c7824c44d19476da
William Liederer <williamliederer@Williams-MacBook-Pro.local>
2020-12-14 10:22:37 -0600

    added state update and clear button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,26 +2,67 @@ import React, {Component} from 'react';
 import API from "./API"
 import Planet from './Planet';
 
+const initialState = {
+    discoveryYear:"",
+    discoveryMethod:"",
+    hostName:"",
+    discoveryFacility:"",
+    searchText:"",
+    selection:"",
+    error:"",
+    planets:[]
+}
+
 
 
 class Search extends Component {
-    state = {
-        discoveryYear:"",
-        discoveryMethod:"",
-        hostName:"",
-        discoveryFacility:"",
-        searchText:"",
-        selection:"",
-        error:"",
-        planets:[]
+    constructor(props) {
+        super(props)
+        this.state=initialState;
+
     }
     
+    handleClear = (e) => {
+        this.setState({state:initialState})
+    }
 
-    handleChange = (event) => {
+    handleSelect = (event) => {
         const {name, value} = event.target
         this.setState({
             [name]:value
         })
+        this.setState({error:""});
+        this.setState({searchText:""})
+    }
+
+    handleInput = (event) => {
+        const {name, value} = event.target
+        this.setState({
+            [name]:value
+        })
+        if(this.state.selection === "hostName"){
+            this.setState((state) => ({
+                hostName: state.searchText
+            })
+            )
+        } else if(this.state.selection === "discoveryYear"){
+            this.setState((state) => ({
+                discoveryYear: state.searchText
+            })
+            )
+        } else if(this.state.selection === "discoveryFacility"){
+            this.setState((state) => ({
+                discoveryFacility: state.searchText
+            })
+            )
+        } else if(this.state.selection === "discoveryMethod"){
+            this.setState((state) => ({
+                discoveryMethod: state.searchText
+            })
+            )
+        } else {
+            this.setState({error:"provide search selection"})
+        }
     }
 
     validate=()=> {
@@ -31,7 +72,7 @@ class Search extends Component {
             this.setState({error:searchError});
           return false;
         } 
-       
+        this.setState({error:""});
         return true;
     }
 
@@ -42,10 +83,9 @@ class Search extends Component {
         const isValid = this.validate();
         
         if(isValid){
-
         API.post('/searchPlanets', {
             
-            hostName:this.state.searchText,
+            hostName:this.state.hostName,
             discoveryYear:this.state.discoveryYear,
             discoveryMethod:this.state.discoveryMethod,
             discoveryFacility:this.state.discoveryFacility
@@ -70,16 +110,19 @@ class Search extends Component {
                 Search componenet
                 <form onSubmit={this.handleSubmit}>
                     <div>
-                        <select name="selection" value={this.state.selection} onChange={this.handleChange}>
+                        <select name="selection" value={this.state.selection} onChange={this.handleSelect}>
                             <option></option>
                             <option value="discoveryYear">discoveryYear</option>
                             <option value="discoveryMethod">discoveryMethod</option>
                             <option value="hostName">hostName</option>
                             <option value="discoveryFacility">discoveryFacility</option>
                         </select>
-                        <input type="text" name="searchText" placeholder="search" value={this.state.searchText} onChange={this.handleChange}/>
+                        <input type="text" name="searchText" placeholder="search" value={this.state.searchText} onChange={this.handleInput}/>
                     </div>
                     <button>search button</button>
+                </form>
+                <form onSubmit={this.handleClear}>
+                <button>clear</button>
                 </form>
         <div style={{color:"red"}}>{this.state.error}</div>
         
